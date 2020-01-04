bf868005181e128195ea5644ed7482d46595cfc6
Liia Flinkman <liia.flinkman@gmail.com>
2020-01-04 02:24:22 +0100

    tehtävä 3.2 ja 3.3

--- index.js
+++ index.js
@@ -8,31 +8,46 @@ let names = [
         name: "Arto Hellas",
         number: "050-123456",
         id: 1
-        
+
     },
     {
         name: "Ada Lovelace",
         number: "39-5323523",
-        id: 4
+        id: 2
     },
     {
         name: "Dan Abramod",
-        numberid: "12-43-234345",
+        number: "12-43-234345",
         id: 3
     },
     {
         name: "Mary Poppendieck",
-        numberid: "39-34-6423122",
-        id: 5
+        number: "39-34-6423122",
+        id: 4
     }
 ]
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
+app.get('/info', (req, res) => {
+    var maara = names.length;
+    const date = Date()
+    res.send(`<p>Phonebook has info for ${maara} people </p><p>${date}</p>`)
+})
 
 app.get('/api/persons', (req, res) => {
     res.json(names)
+})
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const name = names.find(name => name.id === id)
+
+    if (name) {
+        response.json(name)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
