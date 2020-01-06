2ea77af2ca38dcde71b6338cbf53cc0eee1c9cd1
Liia Flinkman <liia.flinkman@gmail.com>
2020-01-07 00:59:49 +0100

    tehtävät 3.9, 3.10 ja 3.11

---
+++ backend/precache-manifest.7c93fd346157486610740cefdf3bf743.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1531a98f677d5fd09948e906d83845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "94c7f7c9c08eac876d6f",
+    "url": "/static/js/main.0d8cc1b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
