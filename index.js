5af013b317788573b22fa10a3eb89e4d53554ef5
Liia Flinkman <liia.flinkman@gmail.com>
2020-01-07 01:24:39 +0100

    herokuun

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 app.use(express.static('build'))
 
+
 let names = [
     {
         name: "Arto Hellas",
